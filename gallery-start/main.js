bf51f5fdd6d8b0fc16ce3f4e1b038e88f325a2b4
chelsea12x2 <chelsea12x2@gmail.com>
2023-03-03 16:39:49 -0500

    gallery

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -6,13 +6,35 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < images.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', images[i]);
+    newImage.setAttribute('alt', images[i]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    })
+};
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const className = btn.getAttribute('class');
+    if (className === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else if (className === 'light') {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
+  
